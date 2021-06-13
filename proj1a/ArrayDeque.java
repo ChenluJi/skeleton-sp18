6eb319f471b1137c7f7e04dd2f25d08a4ae0a48e
jichenlu <admin@jichenlu.local>
2021-06-12 23:00:13 -0400

    project1

---
+++ proj1a/ArrayDeque.java
@@ -0,0 +1,145 @@
+public class ArrayDeque<T> {
+    private T[] items;
+    private int size;
+    /** Circular ArrayDeque */
+    private int nextFirst;
+    private int nextLast;
+    /** constructor: Creates an empty array deque. */
+    public ArrayDeque() {
+        items = (T[]) new Object[8];
+        size = 0;
+        nextFirst = 0;
+        nextLast = 0;
+    }
+    /** constructor: Creates a deep copy of other. */
+    public ArrayDeque(ArrayDeque other) {
+        size = other.size();
+        Object[] a = new Object[other.items.length];
+        System.arraycopy(other.items, 0, a, 0, other.items.length);
+        items = (T[]) a;
+        nextFirst = other.nextFirst;
+        nextLast = other.nextLast;
+    }
+    private int getPrevIndex(int idx){
+        int prevIdx = idx - 1;
+        if (prevIdx<0) {
+            prevIdx += items.length;
+        }
+        return prevIdx;
+    }
+    private int getNextIndex(int idx){
+        int nextIdx = idx + 1;
+        if (nextIdx>=items.length) {
+            nextIdx -= items.length;
+        }
+        return nextIdx;
+    }
+    private int getNextNthIndex(int idx, int n){
+        int nextNthIdx = idx + n;
+        if (nextNthIdx>=items.length) {
+            nextNthIdx -= items.length;
+        }
+        return nextNthIdx;
+    }
+    /** resize current items length to 2*length */
+    private T[] resize(T[] array) {
+        T[] a = (T[]) new Object[size*2];
+        //TODO: check arraycopy
+        System.arraycopy(array, getPrevIndex(nextFirst), a, 0, array.length-getPrevIndex(nextFirst));
+        System.arraycopy(array, 0, a, array.length-getPrevIndex(nextFirst), getNextIndex(nextLast));
+        nextFirst = a.length - 1;
+        nextLast = size;
+        return a;
+    }
+    /** downsize current items length: For arrays of length 16 or more,
+     * your usage factor should always be at least 25%.
+     * For smaller arrays, your usage factor can be arbitrarily low */
+    private T[] downSize(T[] array) {
+        T[] a = (T[]) new Object[size*4];
+        // TODO: check arraycopy
+        System.arraycopy(array, getPrevIndex(nextFirst), a, 0, array.length-getPrevIndex(nextFirst));
+        System.arraycopy(array, 0, a, array.length-getPrevIndex(nextFirst), getNextIndex(nextLast));
+        nextFirst = a.length - 1;
+        nextLast = size;
+        return a;
+    }
+
+    /** Adds an item of type T to the front of the deque */
+    public void addFirst(T f) {
+        if (size== items.length) {
+            items = resize(items);
+        }
+        items[nextFirst] = f;
+        nextFirst = getPrevIndex(nextFirst);
+        size += 1;
+    }
+    /** Adds an item of type T to the back of the deque */
+    public void addLast(T l) {
+        if (size== items.length) {
+            items = resize(items);
+        }
+        items[nextLast] = l;
+        nextLast = getNextIndex(nextLast);
+        size += 1;
+    }
+    /** Returns true if deque is empty, false otherwise */
+    public boolean isEmpty() {
+        return size==0;
+    }
+    /** Returns the number of items in the deque */
+    public int size() {
+        return size;
+    }
+    /** Prints the items in the deque from first to last, separated by a space.
+     * Once all the items have been printed, print out a new line */
+    public void printDeque() {
+        int idx = getNextIndex(nextFirst);
+        while (idx != nextLast) {
+            System.out.print(items[idx]);
+            System.out.print(" ");
+            idx = getNextIndex(idx);
+        }
+        System.out.print("\n");
+    }
+    /** Removes and returns the item at the front of the deque.
+     * If no such item exists, returns null. For arrays of length 16 or more,
+     * your usage factor should always be at least 25%.
+     * For smaller arrays, your usage factor can be arbitrarily low */
+    public T removeFirst() {
+        T first = items[getNextIndex(nextFirst)];
+        items[getNextIndex(nextFirst)] = null;
+        if (size > 0) {
+            size -= 1;
+        }
+        nextFirst = getNextIndex(nextFirst);
+        if (size>= 16) {
+            items = downSize(items);
+        }
+        return first;
+    }
+    /** Removes and returns the item at the back of the deque.
+     * If no such item exists, returns null. For arrays of length 16 or more,
+     * your usage factor should always be at least 25%.
+     * For smaller arrays, your usage factor can be arbitrarily low */
+    public T removeLast() {
+        T last = items[getPrevIndex(nextLast)];
+        items[getPrevIndex(nextLast)] = null;
+        if (size > 0) {
+            size -= 1;
+        }
+        nextLast = getPrevIndex(nextLast);
+        if (size>= 16) {
+            items = downSize(items);
+        }
+        return last;
+    }
+    /** Gets the item at the given index, where 0 is the front, 1 is the next item, and so forth.
+     * If no such item exists, returns null. Must not alter the deque! */
+    public T get(int index) {
+        if (index+1>size) {
+            return null;
+        }
+        int first = getPrevIndex(nextFirst);
+        return items[getNextNthIndex(first, index)];
+    }
+}
---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,116 @@
+//package PACKAGE_NAME;
+public class LinkedListDeque<T> {
+    public class Node {
+        public T item;
+        public Node prev;
+        public Node next;
+        public Node(T i, Node p, Node n) {
+            item = i;
+            prev = p;
+            next = n;
+        }
+    }
+    final private Node sentinel;
+    private int size;
+    /** constructor: Creates an empty linked list deque. */
+    public LinkedListDeque() {
+        sentinel = new Node(null, null, null);
+        sentinel.prev = sentinel;
+        sentinel.next = sentinel;
+        size = 0;
+    }
+    /** constructor: Creates a deep copy of other */
+    public LinkedListDeque(LinkedListDeque other) {
+        sentinel = new Node(null, null, null);
+        sentinel.prev = sentinel;
+        sentinel.next = sentinel;
+        size = 0;
+        for (int i=0; i<other.size(); i++) {
+            addLast((T) other.get(i));
+        }
+    }
+    /** Adds an item of type T to the front of the deque */
+    public void addFirst(T item) {
+        Node first = new Node(item, sentinel, null);
+        sentinel.next.prev = first;
+        first.next = sentinel.next;
+        sentinel.next = first;
+        size += 1;
+    }
+    /** Adds an item of type T to the back of the deque */
+    public void addLast(T item) {
+        Node last = new Node(item, null, sentinel);
+        sentinel.prev.next = last;
+        last.prev = sentinel.prev;
+        sentinel.prev = last;
+        size += 1;
+    }
+    /** Returns true if deque is empty, false otherwise */
+    public boolean isEmpty() {
+        return size==0;
+    }
+    /** Returns the number of items in the deque */
+    public int size() {
+        return size;
+    }
+    /** Prints the items in the deque from first to last, separated by a space.
+     * Once all the items have been printed, print out a new line */
+    public void printDeque() {
+        Node current = sentinel.next;
+        for (int i=0; i<size; i++) {
+            System.out.print(current.item.toString());
+            System.out.print(" ");
+            current = current.next;
+        }
+        System.out.print("\n");
+    }
+    /** Removes and returns the item at the front of the deque.
+     * If no such item exists, returns null. */
+    public T removeFirst() {
+        T first = sentinel.next.item;
+        sentinel.next = sentinel.next.next;
+        sentinel.next.prev = sentinel;
+        if (size > 0) {
+            size -= 1;
+        }
+        return first;
+    }
+    /** Removes and returns the item at the back of the deque.
+     * If no such item exists, returns null. */
+    public T removeLast() {
+        T last = sentinel.prev.item;
+        sentinel.prev = sentinel.prev.prev;
+        sentinel.prev.next = sentinel;
+        if (size > 0) {
+            size -= 1;
+        }
+        return last;
+    }
+    /** Gets the item at the given index, where 0 is the front, 1 is the next item, and so forth.
+     * If no such item exists, returns null. Must not alter the deque! */
+    public T get(int index) {
+        if (index+1>size) {
+            return null;
+        }
+        Node current = sentinel.next;
+        for (int i=0; i<index; i++) {
+            current = current.next;
+        }
+        return current.item;
+    }
+    /** Same as get, but uses recursion */
+    private T getRecursive(int index, Node current) {
+        if (index==0) {
+            return current.item;
+        } else {
+            return getRecursive(index-1, current.next);
+        }
+    }
+    public T getRecursive(int index) {
+        if (index + 1 > size) {
+            return null;
+        }
+        Node current = sentinel.next;
+        return getRecursive(index, current);
+    }
+}
